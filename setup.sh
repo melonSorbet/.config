d8c3ff7bad28a4ce1ae31e7cc32fc0c36f624e82
melonSorbet <kilicyunusemre678@gmail.com>
2026-01-14 23:29:15 +0100

    zsh out of home manager

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cat > .zshrc << "EOF"
+# Enable completion
+autoload -Uz compinit
+compinit
+# Autosuggestions
+for file in /nix/store/*-zsh-autosuggestions-*/share/zsh-autosuggestions/zsh-autosuggestions.zsh(N); do
+  source "$file"
+  break
+done
+# Syntax highlighting
+for file in /nix/store/*-zsh-syntax-highlighting-*/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh(N); do
+  source "$file"
+  break
+done
+# Aliases
+alias ll="ls -l"
+alias update="sudo dnf update && home-manager switch"
+alias editconfig="nvim /home/user/.config/home-manager/home.nix"
+# Oh My Zsh
+for dir in /nix/store/*-oh-my-zsh-*/share/oh-my-zsh(N); do
+  export ZSH="$dir"
+  ZSH_THEME="agnoster"
+  plugins=(git)
+  source "$ZSH/oh-my-zsh.sh"
+  break
+done
+# History
+HISTSIZE=10000
+SAVEHIST=10000
+HISTFILE=~/.zsh_history
+eval "$(zoxide init --cmd cd zsh)"
+EOF
